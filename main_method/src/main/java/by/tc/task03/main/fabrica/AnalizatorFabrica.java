4b81b39bb4dc662ae8d48c539fa4b0f89eb015dc
OSmol <Olga_Smolyakova@outlook.com>
2018-02-19 11:38:55 +0300

    Update AnalizatorFabrica.java

--- main_method/src/main/java/by/tc/task03/main/fabrica/AnalizatorFabrica.java
+++ main_method/src/main/java/by/tc/task03/main/fabrica/AnalizatorFabrica.java
@@ -5,7 +5,9 @@ import main.java.analizator.Analizator;
 
 import java.io.FileInputStream;
 
-public class AnalizatorFabrica {
+public class AnalizatorFabrica {// ты пользуешься джаркой, и создаешь фарбику, поставляющую ее объекты
+    // ЗАЧЕМ ты это делаешь?????? у тебя задача требует изменения подхода создания объектов
+    // не уподобляетесь братьям нашим меньшим - включай голову, всю, а не просто используй набор выученных трюков
 
     private static final AnalizatorFabrica instance = new AnalizatorFabrica();
 
